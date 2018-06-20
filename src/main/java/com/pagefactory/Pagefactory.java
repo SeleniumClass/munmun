330ee833d2d85eb9c91e08a9754c47c95c0e0d32
munmun <munmun@DESKTOP-VMJR54F.fios-router.home>
2018-06-19 20:26:09 -0400

    page factory

--- src/main/java/com/pagefactory/Pagefactory.java
+++ src/main/java/com/pagefactory/Pagefactory.java
@@ -6,18 +6,41 @@ import org.openqa.selenium.support.How;
 
 public class Pagefactory {
 	
-	@FindBy(how = How.XPATH, using = "(//*[contains(text(),Sign In')])[2]")
-	private WebElement signInbtn;
+	@FindBy(how = How.XPATH, using = "(//*[contains(text(),'My Account')])")
+	private WebElement myAccount;
 
-	public WebElement getSignInbtn() {
-		return signInbtn;
-	}
+		@FindBy(how = How.XPATH, using = "(//*[contains(name,'email_address')])")
+		private WebElement email_address;
 
-	public static void main(String[] args) {
+			@FindBy(how = How.XPATH, using = "(//*[contains(name,'password')])")
+			private WebElement password;
+			
+				@FindBy(how = How.XPATH, using = "(//*[contains(@class,'ui-button-text')][10]]")
+				private WebElement SignIn;
+
+				public WebElement getMyAccount() {
+					return myAccount;
+				}
+
+				public WebElement getEmail_address() {
+					return email_address;
+				}
+
+				public WebElement getPassword() {
+					return password;
+				}
+
+				public WebElement getSignIn() {
+					return SignIn;
+				}
+
+					
+				
+	
 		
 	}
 
 
 	
 
-}
+
